344780e61ee4f482f49a277f4e47a0023cac65b5
MatasSioma <matassemenas@gmail.com>
2024-12-10 22:58:22 +0200

    kainos nustatymas uzsakymui

--- djangoApp/retail/urls.py
+++ djangoApp/retail/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('', views.index, name='index'),
 
     path('send-order/', views.send_order, name='send_order'),
-    path('get-events/', views.get_order_events, name='get_events'),
+    path('set-price/', views.set_price, name='set_price'),
+    # path('pay/', views.pay, name='pay'),
+    # path('cancel/', views.cancel, name='cancel'),
+    # path('mark-delivered/', views.mark_delivered, name='mark_delivered'),
+
 ]
--- djangoApp/retail/views.py
+++ djangoApp/retail/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect, JsonResponse
 from web3 import Web3
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
+from datetime import datetime
 import json
 
 # Connect to the Ethereum node
@@ -31,9 +32,42 @@ def index(request):
         with open(f'{settings.BASE_DIR}/retail/products.json', 'r') as productsf:
             products = json.load(productsf)
         orderAmount = contract.functions.orderCount().call()
+
+        statusTrans = {
+            0: "None",
+            1: "Ordered",
+            2: "Priced",
+            3: "Paid",
+            4: "Delivered",
+            5: "Cancelled"
+        }
+
         orders = []
-        for orderId in range(orderAmount):
-            orders.append(contract.functions.orders(orderId).call())
+        for orderId in range(1, orderAmount+1):
+            order = contract.functions.orders(orderId).call()
+            order = {
+                "orderId": orderId,
+                "retailer": order[0],
+                "courier":order[1],
+                "product": order[2],
+                "quantity": order[3],
+                "orderPrice": order[4],
+                "shipmentPrice": order[5],
+                "grandTotal": order[6],
+                "deliveryDate": order[7],
+                "otherData": order[8],
+                "status": statusTrans[order[9]],
+            }
+            for product in products:
+                if product["title"] == order["product"]:
+                    order["weight"] = order["quantity"] * product["weight"]
+                    break
+            #1 Wei = 0.00001971 EUR   2024-12-10
+            if order["grandTotal"] != 0:
+                order["price"] = round(order["grandTotal"] * 0.00001971, 2)
+                order["priceETH"] = web3.from_wei(order["grandTotal"],'ether')
+            orders.append(order)
+
         return render(request, "index.html", {"products": products, "popUps": popUps,"orders": orders})
 
 @csrf_exempt
@@ -69,18 +103,53 @@ def send_order(request):
             return HttpResponseRedirect(f"/?message={json.dumps(message).replace("'", '"')}")
         except Exception as e:
             return JsonResponse({"error": str(e)}, status=500)
-        
-def get_order_events(request):
-    try:
-        events = contract.events.OrderCreated.create_filter(from_block="latest").get_all_entries()
-        response = [
+
+@csrf_exempt
+def set_price(request):
+    if request.method == "POST":
+        try:
+            orderId = int(request.POST.get("id"))
+            productTitle = request.POST.get("product")
+            quantity = int(request.POST.get("quantity"))
+            shipmentPrice = float(request.POST.get("price"))
+            deliveryDate = int(datetime.fromisoformat(request.POST.get("date")).timestamp())
+            courierAddress = request.POST.get("courier-address")
+            
+
+            with open(f'{settings.BASE_DIR}/retail/products.json', 'r') as productsf:
+                products = json.load(productsf)
+
+            for product in products:
+                if productTitle == product["title"]:
+                    orderPrice = round(product["price"] * quantity, 2)
+                    break
+            
+            orderPrice = int(round(orderPrice / 0.00001971, 0))
+            shipmentPrice = int(round(shipmentPrice / 0.00001971, 0))
+
+            transaction = contract.functions.setPrices(
+                orderId, orderPrice, shipmentPrice, deliveryDate, courierAddress
+            ).build_transaction({
+                'from': MANUFACTURER_ADDRESS,
+                'gas': 2000000,
+                'nonce': web3.eth.get_transaction_count(MANUFACTURER_ADDRESS),
+            })
+            signed_tx = web3.eth.account.sign_transaction(transaction, config["MANUFACTURER_PRIVATE_KEY"])
+            tx_hash = web3.eth.send_raw_transaction(signed_tx.raw_transaction)
+            web3.eth.wait_for_transaction_receipt(tx_hash)
+
+            events = contract.events.PriceSent.create_filter(from_block="latest").get_all_entries()
+
+            message = [
             {
                 "orderId": event.args.orderId,
-                "retailer": event.args.retailer,
-                "product": event.args.product,
-                "quantity": event.args.quantity,
+                "orderPrice": round(orderPrice*0.00001971,2),
+                "shipmentPrice": round(shipmentPrice*0.00001971,2),
+                "grandTotal": round(orderPrice*0.00001971 + shipmentPrice*0.00001971,2),
+                "type": "Priced"
             } for event in events
-        ]
-        return JsonResponse({"events": response})
-    except Exception as e:
-        return JsonResponse({"error": str(e)}, status=400)
+            ]
+
+            return HttpResponseRedirect(f"/?message={json.dumps(message).replace("'", '"')}")
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=500)
